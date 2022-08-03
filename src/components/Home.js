53ab0abd7ea41c6f3f221a169462fb949621c771
Maung Htike <maunghtike1999@gmail.com>
2022-08-03 04:56:42 -0700

    adjusted css styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import bg from '../assets/homebg.webm';
-import { Button, Typography, Stack } from "@mui/material";
+import { Button, Typography, Divider, Stack } from "@mui/material";
 import AboutMe from "../components/AboutMe";
 import Projects from "../components/Projects";
 import Contacts from "../components/Contacts";
@@ -10,7 +10,7 @@ import resume from "../assets/resume.pdf";
 export default function Home({ setPage }) {
 
     return (
-        <section className='homepage'>
+        <section className='gifbg'>
 
             <Typography className="homeLogo" component="div" sx={{ position: "absolute", top: "0", left: "0", padding: 1.5, marginTop: "3%", marginLeft: "3%", zIndex: 105 }}>
                 <img src={logo} alt="website logo" width="200px" />
@@ -23,11 +23,11 @@ export default function Home({ setPage }) {
             <div className='text'>
                 <h2>Maung Htike</h2>
                 <p>Full stack web developer (MERN) educated at UC Berkeley Ext. Eager to bring my experience from the military and civilian side to make apps that matter</p>
-                <Stack spacing={2} direction="row">
+                <Stack spacing={3} direction={{ xs: 'column', sm: 'row' }}>
                     <Button variant="contained" color="warning" onClick={() => setPage(() => <AboutMe />)}>About Me</Button>
                     <Button variant="contained" color="warning" onClick={() => setPage(() => <Projects />)}>Projects</Button>
                     <Button variant="contained" color="warning" onClick={() => setPage(() => <Contacts />)}>Contact</Button>
-                    <a style={{ textDecoration: "none" }} href={resume} target="_blank" rel="noreferrer"><Button color="warning" variant="contained" >Resume</Button></a>
+                    <Button href={resume} target="_blank" rel="noreferrer" color="warning" variant="contained" >Resume</Button>
                 </Stack>
             </div>
         </section>
