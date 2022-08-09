a2d0940f028e22326866d18d8f1bd7765510e14b
Maung Htike <maunghtike1999@gmail.com>
2022-08-09 02:03:32 -0700

    refactor home page to tailwind css and mobile ok

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,33 @@
 import React, { useState } from 'react';
-import bg from '../assets/homebg.webm';
-import { Button, Typography, Stack } from "@mui/material";
+import bg from '../assets/homebg.gif';
 import AboutMe from "../components/AboutMe";
 import Projects from "../components/Projects";
-import Contacts from "../components/Contacts";
-import logo from "../assets/logo.png";
-import resume from "../assets/resume.pdf";
+import Contact from "../components/Contacts";
 
 export default function Home({ setPage }) {
 
 
     return (
-        <section className='gifbg'>
+        <>
+            {/* background gif */}
+            <section className='absolute w-screen min-h-screen flex items-center bg-black color-white -z-50'>
+                <img src={bg} alt="background" className='absolute top-0 left-0 w-screen h-screen object-cover opacity-90' />
+                <div className="absolute top-0 left-0 w-screen h-screen bg-[#9a6724] mix-blend-overlay"></div>
+            </section>
 
-            <Typography className="homeLogo" component="div" sx={{ position: "absolute", top: "0", left: "0", padding: 1.5, marginTop: "3%", marginLeft: "3%", zIndex: 105 }}>
-                <img src={logo} alt="website logo" width="200px" />
-            </Typography>
-
-            <video autoPlay muted playsInline loop width="100%" src={bg} type="video/webm"></video>
-
-            <div className="overlay"></div>
-
-            <div className='text'>
-                <h2>Maung Htike</h2>
-                <p>Full stack web developer (MERN) educated at UC Berkeley Ext. Eager to bring my experience from the military and civilian side to make apps that matter</p>
-                <Stack spacing={3} direction={{ xs: 'column', sm: 'row' }}>
-                    <Button variant="contained" color="warning" onClick={() => setPage(() => <AboutMe />)}>About Me</Button>
-                    <Button variant="contained" color="warning" onClick={() => setPage(() => <Projects />)}>Projects</Button>
-                    <Button variant="contained" color="warning" href="https://www.linkedin.com/in/maung-htike-482b4b143/">Contact</Button>
-                    <Button href={resume} target="_blank" rel="noreferrer" color="warning" variant="contained" >Resume</Button>
-                </Stack>
-            </div>
-        </section>
+            {/* textbox */}
+            <section>
+                <div className="text-center bg-[#0000004b] text-gray-800 py-24 px-6 grid md:absolute md:inset-1/4 h-screen sm:h-fit">
+                    <h1 className="text-3xl sm:text-4xl md:text-5xl xl:text-5xl 2xl:text-6xl font-extrabold tracking-tight text-white uppercase ">Maung Htike</h1>
+                    <p className='text-xl my-5 text-white text-center mx-10 my-10 max-w-fit'>Full stack web developer (MERN) educated at UC Berkeley. Eager to bring my experience from the military and civilian side to make apps that matter.</p>
+                    <div className="grid gap-4 md:flex justify-center">
+                        <button className="inline-block px-6 py-2.5 bg-orange-500 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-700 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" onClick={() => setPage(() => <AboutMe />)}>About Me</button>
+                        <button className="inline-block px-6 py-2.5 bg-orange-500 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-700 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" onClick={() => setPage(() => <Projects />)}>Projects</button>
+                        <button className="inline-block px-6 py-2.5 bg-orange-500 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-700 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" onClick={() => setPage(() => <Contact />)}>Contact</button>
+                    </div>
+                </div>
+            </section>
+        </>
     );
 }
 
