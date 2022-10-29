1bad66fef42294921b3774ae6db2406887498af2
Maung Htike <maunghtike1999@gmail.com>
2022-10-29 04:05:14 -0700

    deleted extra btns

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import bggif from '../assets/homebg.gif';
-import AboutMe from "../components/AboutMe";
-import Contact from "../components/Contacts";
 
 
 export default function Home({ setPage, setMenuDrawer, setHomeCheck }) {
@@ -23,10 +21,8 @@ export default function Home({ setPage, setMenuDrawer, setHomeCheck }) {
                     <h1 className="text-3xl sm:text-4xl md:text-5xl xl:text-5xl 2xl:text-6xl font-extrabold tracking-tight text-white uppercase" style={{ textShadow: "0px 0px 5px black" }}>Maung Htike</h1>
                     <p className='text-xl my-5 text-white text-center mx-10 my-10 max-w-fit' style={{ textShadow: "0px 0px 5px black" }}>Full stack web developer (MERN) educated at UC Berkeley. Eager to bring my experience from the military and civilian side to make apps that matter.</p>
                     <div className="grid gap-4 md:flex justify-center">
-                        <button className="inline-block px-6 py-2.5 bg-orange-500 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-700 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" onClick={() => { setPage(() => <AboutMe />); setHomeCheck(false) }} style={{ textShadow: "0px 0px 3px black" }}>About Me</button>
-                        <button className="inline-block px-6 py-2.5 bg-orange-500 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-700 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" onClick={() => { setMenuDrawer(true) }} style={{ textShadow: "0px 0px 3px black" }}>MENU BTN
+                        <button className="inline-block px-6 py-2.5 bg-orange-500 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-700 active:shadow-lg transition duration-150 ease-in-out h-10" data-mdb-ripple="true" data-mdb-ripple-color="light" onClick={() => { setMenuDrawer(true) }} style={{ textShadow: "0px 0px 3px black" }}>MENU
                         </button>
-                        <button className="inline-block px-6 py-2.5 bg-orange-500 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-orange-600 hover:shadow-lg focus:bg-orange-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-700 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" onClick={() => { setPage(() => <Contact />); setHomeCheck(false) }} style={{ textShadow: "0px 0px 3px black" }}>Contact</button>
                     </div>
                 </div>
             </section>
